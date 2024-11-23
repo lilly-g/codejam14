7ce0646be19431f7d061037c2acdc378ee31f70c
Zoe Zhao <151393883+zoejzhao@users.noreply.github.com>
2024-11-23 17:01:06 -0500

    unique join code generation
    adds a unique 6-character join code to "create group outing" option.
    
    fixed scope error in action.js (ports)

--- backend/controllers/outingController.js
+++ backend/controllers/outingController.js
@@ -4,6 +4,43 @@ const Outing = require("../models/outingModel");
 exports.createNewOuting = async (req, res) => {
     // get form data
     const { newGroupName, newAdminName, newAdminPass } = req.body;
+    
+    // generate random join code
+    function genJoinCode() {
+        let result = ''
+        async function checkUnique(str){
+            try {
+                const jC = await Outing.findOne({ joinCode: str }); // Equality query
+                if (jC) {
+                    return false;
+                } else {
+                    return true;
+                }
+            } catch (error) {
+                console.error('Error querying the database:', error);
+            }
+        }
+        do{
+            ;
+            const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+            const charactersLength = characters.length;
+            let counter = 0;
+            while (counter < 6) {
+                result += characters.charAt(Math.floor(Math.random() * charactersLength));
+                counter += 1;
+            }
+        } while(checkUnique(result) === false)
+        
+
+        return result;
+    }
+    let joinCode = genJoinCode();
+    
+
+
+
+
+    
 
     // make new entry
     Outing.create({
@@ -13,7 +50,7 @@ exports.createNewOuting = async (req, res) => {
             adminPass: newAdminPass
         },
         status: "accepting",
-        joinCode: "joincode"
+        joinCode: joinCode
     })
 
     res.status(200).json({ "message": "success" });
