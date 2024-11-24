bb02ea506cdb1e411c2e1a5574c9265420b40456
Lilly Gao <lillygao@Lillys-MacBook-Air.local>
2024-11-23 20:29:27 -0500

    frontend for landing page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -11,29 +11,29 @@ import LoginForm from "@/components/LoginForm";
 export default function Page() {
 
     return (
-        <div className="font-black">
-            <div className="basis-1/3 p-8">
-                <h1 className="text-4xl">extinguish inefficiency.</h1>
-                <br />
-                <p className="font-body">Tired of indecisive planning?</p>
-                <br />
-                <p className="font-body">Scared to have your ideas shot down?</p>
-                <br />
-                <p className="font-body">Coordinate group plans with ease using Sociouts' interactive and collaborative selection technology.</p>
-                <br />
-                <p className="font-body">Join a group, select your area, and start swiping!</p>
-
-            </div>
-
-            <h1 className="text-center">
-                <Link href="/logInInfo">
-                    Get Started
-                </Link>
-            </h1>
-
-            <img src="codejam14/frontend/public/images/friendparty.png" alt="friends partying"></img>
-
+      <div className="font-display text-lg flex flex-col justify-start h-screen border-violet-900">
+          <img src="/images/party3.png" alt="friends partying" className="w-full h-1/2 object-cover shadow-md opacity-100 transition-opacity duration-1000 hover:opacity-80" />
+        <div className="basis-1/3 pl-10">
+            <br />
+            <h1 className="text-4xl">extinguish inefficiency.</h1>
+            <br />
+            <p className="font-body">Tired of indecisive planning?</p>
+            <br />
+            <p className="font-body">Scared to have your ideas shot down?</p>
+            <br />
+            <p className="font-body">Coordinate group plans with ease using interactive and collaborative selection technology.</p>
+            <br />
+            <p className="font-body">Join a group, select your area, and start swiping!</p>
+            <br />
         </div>
+        
+        <button className="bg-purple-900 hover:bg-purple-700 text-white font-body py-2 px-3 object-center">
+            <Link href="/logInInfo" className="btn btn-blue">
+                Get Started
+            </Link>
+        </button>
+    </div>
+  
     );
 
 }
