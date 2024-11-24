65f6ddc64ba45bd8f474b21aa4d23f3f01fb682a
Lilly Gao <lillygao@Lillys-MacBook-Air.local>
2024-11-24 00:55:59 -0500

    edited login page frontend

--- frontend/src/app/logInInfo/page.js
+++ frontend/src/app/logInInfo/page.js
@@ -14,17 +14,18 @@ import LoginForm from "@/components/LoginForm";
 export default function LoginPage() {
     return (
         <div className="bg-purple-300">
-            <div className="flex pl-2 pt-2">
+            <div className="flex items-center pl-2 pt-2">
                 <div className="border-2 border-purple-800">
                     <Image
                         src="/images/logo.png"
                         alt="Example Image"
-                        width={60} 
-                        height={50}
+                        width={30} 
+                        height={30}
                     />
                 </div>
-                <p className="font-display text-purple-900 pt-10 pl-2">sociouts</p>
+                <p className="font-display text-purple-900 pl-2">sociouts</p>
             </div>
+
             <div className="min-h-screen flex items-center justify-center">
                 <div className="flex space-x-8">
                     <div className="w-96 p-8 border-4 border-purple-800 rounded-lg bg-purple-50">
