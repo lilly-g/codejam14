8ad81937cd2d1a2a6b5da66a870c1422b43c0fcb
Lilly Gao <lillygao@Lillys-MacBook-Air.local>
2024-11-24 06:51:05 -0500

    edited results frontend

--- frontend/src/app/results/page.js
+++ frontend/src/app/results/page.js
@@ -5,22 +5,23 @@ import Link from 'next/link';
 export default function Page() {
 
     return (
-        <div className="flex flex-col items-center space-y-8 bg-purple-100 min-h-screen">
+        <div className="flex flex-col items-center space-y-3 bg-purple-100 min-h-screen">
             <div className="text-center">
                 <br />
                 <br />
-                <h1 className="font-display text-3xl text-purple-900">the results are out.</h1>
-                <p className="text-lg text-purple-900">here are your top three.</p>
+                <h1 className="font-display text-5xl text-purple-900 font-black">the results are out.</h1>
+                <br></br>
+                <p className="text-2xl text-purple-900 font-semibold">here are your top three.</p>
             </div>
 
-            <div className="grid grid-cols-3 gap-8 p-8">
+            <div className="grid grid-cols-3 gap-6 p-8">
                 <div className="bg-purple-900 rounded-lg shadow-lg p-8 h-150 overflow-auto">
                     <h2 className="text-white text-2xl font-bold mb-4">Sammi & Soupe Dumpling</h2>
                     <p className="text-white">Simply appointed space known for a variety of soup dumplings, along with noodles & Chinese beers.</p>
                     <img src="/images/sammi.png" alt="place1" className="mt-4 rounded-md" />
                 </div>
 
-                <div className="bg-purple-700 rounded-lg shadow-lg p-8 h-150 overflow-auto">
+                <div className="bg-purple-900 rounded-lg shadow-lg p-8 h-150 overflow-auto">
                     <h2 className="text-white text-2xl font-bold mb-4">Chef on Call</h2>
                     <p className="text-white">Homestyle food for delivery, take-out, or to eat in a casual room with wood accents & subway tiles.</p>
                     <img src="/images/chefoncall.png" alt="place2" className="mt-4 rounded-md" />
