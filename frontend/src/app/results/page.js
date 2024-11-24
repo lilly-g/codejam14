27bbb77c2183daa3d0904859d2c15fc33fd94e81
Lilly Gao <lillygao@Lillys-MacBook-Air.local>
2024-11-23 22:35:25 -0500

    frontend results page

--- frontend/src/app/results/page.js
+++ frontend/src/app/results/page.js
@@ -17,19 +17,19 @@ export default function Page(first, second, third) {
                 <div className="bg-purple-900 rounded-lg shadow-lg p-8 w-96 h-96 overflow-auto">
                     <h2 className="text-white text-2xl font-bold mb-4">Sammi & Soupe Dumpling</h2>
                     <p className="text-white">Simply appointed space known for a variety of soup dumplings, along with noodles & Chinese beers.</p>
-                    <img src="/images/example.png" alt="Example" className="mt-4 rounded-md" />
+                    <img src="/images/logo.png" alt="logo" className="mt-4 rounded-md" />
                 </div>
 
                 <div className="bg-purple-700 rounded-lg shadow-lg p-8 h-96 overflow-auto">
                     <h2 className="text-white text-2xl font-bold mb-4">Chef on Call</h2>
                     <p className="text-white">Homestyle food for delivery, take-out, or to eat in a casual room with wood accents & subway tiles.</p>
-                    <img src="/images/example.png" alt="Example" className="mt-4 rounded-md" />
+                    <img src="/images/logo.png" alt="logo" className="mt-4 rounded-md" />
                 </div>
 
                 <div className="bg-purple-900 rounded-lg shadow-lg p-8 h-96 overflow-auto">
                     <h2 className="text-white text-2xl font-bold mb-4">Kinton Ramen</h2>
                     <p className="text-white">We deliver an exceptional dining experience with fresh ingredients and high-quality noodles and broths.</p>
-                    <img src="/images/example.png" alt="Example" className="mt-4 rounded-md" />
+                    <img src="/images/logo.png" alt="logo" className="mt-4 rounded-md" />
                 </div>
             </div>
 
