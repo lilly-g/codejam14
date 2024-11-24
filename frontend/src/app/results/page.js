3dfddc0178e1b7b584d5898231a43c683597c891
Lilly Gao <lillygao@Lillys-MacBook-Air.local>
2024-11-24 00:34:24 -0500

    updated results frontend

--- frontend/src/app/results/page.js
+++ frontend/src/app/results/page.js
@@ -1,5 +1,6 @@
 'use client'
 import React from "react";
+import Link from 'next/link';
 
 export default function Page(first, second, third) {
 
@@ -13,26 +14,30 @@ export default function Page(first, second, third) {
             </div>
 
             <div className="grid grid-cols-3 gap-8 p-8">
-                <div className="bg-purple-900 rounded-lg shadow-lg p-8 w-96 h-96 overflow-auto">
+                <div className="bg-purple-900 rounded-lg shadow-lg p-8 h-96 overflow-auto">
                     <h2 className="text-white text-2xl font-bold mb-4">Sammi & Soupe Dumpling</h2>
                     <p className="text-white">Simply appointed space known for a variety of soup dumplings, along with noodles & Chinese beers.</p>
-                    <img src="/images/logo.png" alt="logo" className="mt-4 rounded-md" />
+                    <img src="/images/sammi.png" alt="place1" className="mt-4 rounded-md" />
                 </div>
 
                 <div className="bg-purple-700 rounded-lg shadow-lg p-8 h-96 overflow-auto">
                     <h2 className="text-white text-2xl font-bold mb-4">Chef on Call</h2>
                     <p className="text-white">Homestyle food for delivery, take-out, or to eat in a casual room with wood accents & subway tiles.</p>
-                    <img src="/images/logo.png" alt="logo" className="mt-4 rounded-md" />
+                    <img src="/images/chefoncall.png" alt="place2" className="mt-4 rounded-md" />
                 </div>
 
                 <div className="bg-purple-900 rounded-lg shadow-lg p-8 h-96 overflow-auto">
                     <h2 className="text-white text-2xl font-bold mb-4">Kinton Ramen</h2>
                     <p className="text-white">We deliver an exceptional dining experience with fresh ingredients and high-quality noodles and broths.</p>
-                    <img src="/images/logo.png" alt="logo" className="mt-4 rounded-md" />
+                    <img src="/images/kinton.png" alt="place3" className="mt-4 rounded-md" />
                 </div>
             </div>
 
-            <p className="text-lg underline">Start a new group</p>
+            <button className="bg-purple-900 hover:bg-purple-700 text-white font-body py-2 px-3 object-center">
+                <Link href="/logInInfo" className="btn btn-blue">
+                    Create a new group
+                </Link>
+            </button>
 
         </div>
     );
